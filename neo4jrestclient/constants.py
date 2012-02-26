fa83fa3f9ed26b4fbd96b0db995a4ac2f9cdc55d
Javier de la Rosa <versae@gmail.com>
2012-02-26 11:41:39 -0500

    Removing __credits__ in favor of AUTHORS file. Updating version number

--- neo4jrestclient/constants.py
+++ neo4jrestclient/constants.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "Javier de la Rosa"
-__credits__ = [
-    "Javier de la Rosa",
-    "Diego Muñoz Escalante, https://github.com/escalant3",
-    "Yashh, https://github.com/yashh",
-    "timClicks, https://github.com/timClicks",
-    "Mark Henderson, https://github.com/emehrkay",
-    "Johan Lundberg, https://github.com/johanlundberg",
-    "Mark Ng, https://github.com/markng",
-    "kesavkolla, https://github.com/kesavkolla",
-    "Andy Denmark, https://github.com/denmark",
-]
 __license__ = "GPL 3"
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 __email__ = "versae@gmail.com"
 __url__ = "https://github.com/versae/neo4j-rest-client"
 __description__ = """Object-oriented Python library to interact with """ \
