e1ec51c6e978bcaa42796af449b48133b08d5b1c
Robin Edwards <robin.ge@gmail.com>
2012-07-24 16:32:10 +0100

    Read test db url from environ

--- neo4jrestclient/tests.py
+++ neo4jrestclient/tests.py
@@ -6,21 +6,19 @@ except:
     import pickle
 
 import client
+import options
+import sys
 import constants
 import request
 import unittest
+import os
 
+NEO4J_URL = os.environ.get('NEO4J_URL', "http://localhost:7474/db/data/")
 
 class NodesTestCase(unittest.TestCase):
-
     def setUp(self):
-        self.url = "http://localhost:7474/db/data/"
+        self.url = NEO4J_URL
         self.gdb = client.GraphDatabase(self.url)
-        # self.secured_url = "http://<instance>.hosted.neo4j.org:7000/db/data/"
-        # self.username = "username"
-        # self.password = "password"
-        # self.gdb = client.GraphDatabase(self.secured_url, username=self.username,
-        #                                 password=self.password)
 
     def tearDown(self):
         import options as clientCacheDebug
